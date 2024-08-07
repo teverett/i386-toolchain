aedf591b95b924ca4639e4b643bbb90353c47e52
Tom Everett <tom@khubla.com>
2024-08-07 16:05:02 -0600

    more

--- toolchain.sh
+++ toolchain.sh
@@ -2,7 +2,7 @@
 # https://pdos.csail.mit.edu/6.828/2016/tools.html#chain
 
 #brew upgrade
-#brew install gcc@12
+#brew install gcc@11
 
 # tool versions
 BINUTILSVERSION=binutils-2.21.1
@@ -10,13 +10,16 @@ GMPVERSION=gmp-5.0.2
 MPFRVERSION=mpfr-4.2.1
 MPCVERSION=mpc-1.1.0
 GCCVERSION=gcc-9.5.0
-GDBVERSION=gdb-9.2
+GDBVERSION=gdb-10.2
 
 # important directories
 HOME=`pwd`
-CELLAR=/usr/local/Cellar/gcc@11/11.4.0/bin
 TOOLCHAINDIR=toolchain
 BUILDDIR=$HOME/$TOOLCHAINDIR/build
+HOMEBREW_PREFIX=`brew config | grep HOMEBREW_PREFIX |  awk '{print $2}'`
+CELLAR=$HOMEBREW_PREFIX/Cellar/gcc@11/11.4.0/bin
+
+echo "HomeBrew is at: $HOMEBREW_PREFIX"
 
 export PREFIX=$HOME/$TOOLCHAINDIR/bin
 export TARGET=i386-elf
@@ -35,6 +38,7 @@ cd $TOOLCHAINDIR
 cd build
 
 # Binutils
+echo "building Binutils"
 if [ ! -f $PREFIX/i386-elf/bin/ld ]; then
     BINUTILSFILE=$BINUTILSVERSION.tar.bz2
     if [ ! -d $BINUTILSVERSION ]; then
@@ -53,6 +57,7 @@ if [ ! -f $PREFIX/i386-elf/bin/ld ]; then
 fi
 
 # GMP
+echo "building GMP"
 if [ ! -f $PREFIX/lib/libgmp.a ]; then
     GMPFILE=$GMPVERSION.tar.bz2
     if [ ! -d $GMPVERSION ]; then
@@ -71,6 +76,7 @@ if [ ! -f $PREFIX/lib/libgmp.a ]; then
 fi
 
 # MPFR
+echo "building MPFR"
 if [ ! -f $PREFIX/lib/libmpfr.a ]; then
     MPFRFILE=$MPFRVERSION.tar.bz2
     if [ ! -d $MPFRVERSION ]; then
@@ -89,6 +95,7 @@ if [ ! -f $PREFIX/lib/libmpfr.a ]; then
 fi
 
 # MPC
+echo "building MPC"
 if [ ! -f $PREFIX/lib/libmpc.a ]; then
     MPCFILE=$MPCVERSION.tar.gz
     if [ ! -d $MPCVERSION ]; then
@@ -107,6 +114,7 @@ if [ ! -f $PREFIX/lib/libmpc.a ]; then
 fi
 
 # GCC
+echo "building GCC"
 if [ ! -f $PREFIX/bin/i386-elf-gcc ]; then
     GCCFILE=$GCCVERSION.tar.gz
     if [ ! -d $GCCVERSION ]; then
@@ -133,19 +141,19 @@ if [ ! -f $PREFIX/bin/i386-elf-gcc ]; then
 fi
 
 # GDB
-GDBFILE=$GDBVERSION.tar.gz
-if [ ! -d $GDBVERSION ]; then
-    if [ ! -f $GDBFILE ]; then
-        wget https://ftp.gnu.org/gnu/gdb/$GDBFILE
-    fi
-    tar zxvf $GDBFILE
-fi
-cd $GDBVERSION
-mkdir build
-cd build
-if [ ! -f Makefile ]; then
-    ../configure --prefix=$PREFIX --target=$TARGET --disable-werror
-fi
-make
-#make install
-cd ..
+    # GDBFILE=$GDBVERSION.tar.gz
+    # if [ ! -d $GDBVERSION ]; then
+    #     if [ ! -f $GDBFILE ]; then
+    #         wget https://ftp.gnu.org/gnu/gdb/$GDBFILE
+    #     fi
+    #     tar zxvf $GDBFILE
+    # fi
+    # cd $GDBVERSION
+    # mkdir build
+    # cd build
+    # if [ ! -f Makefile ]; then
+    #     ../configure --prefix=$PREFIX --target=$TARGET --disable-werror
+    # fi
+    # make
+    # #make install
+    # cd ..
